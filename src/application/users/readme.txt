2c380e3d3c2f8f67c3663c746fee4915162c7469
michael <kurashevmichael@gmail.com>
2022-12-17 16:54:22 +0300

    work_4

--- src/application/users/readme.txt
+++ src/application/users/readme.txt
@@ -1,1 +1,1 @@
-Чтобы прочитать файл лога нужно открыть файл shop-survey/src/log/users_password_log.log
+Для запуска скрипта в терминале ввести bash script_flake8
--- src/application/users/views.py
+++ src/application/users/views.py
@@ -1,3 +1,6 @@
+"""Docstring"""
+
+import logging
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
@@ -9,16 +12,15 @@ from django.views.generic import FormView
 
 from .forms import UserChangePasswordForm
 from .models import User
-import logging
 
 logger = logging.getLogger('django')
+
 
 class UserChangePasswordView(FormView):
 
     """
     Изменение пароля заданного в поле формы "E-mail" пользователя
     """
-
 
     template_name = 'users/change_password.html'
     form_class = UserChangePasswordForm
@@ -30,8 +32,9 @@ class UserChangePasswordView(FormView):
     def dispatch(self, request, *args, **kwargs):
 
         if request.user.email not in settings.ADD_TASK_USERS:
-            logger.info(f'{request.user.username} нет доступа')
-            return HttpResponseForbidden(f'У вас нет прав')
+            text_log = f'{request.user.username} нет доступа'
+            logger.info(text_log)
+            return HttpResponseForbidden('У вас нет прав')
 
         if self.request.method == 'POST':
             request.session['email'] = self.request.POST.get('email')
@@ -41,21 +44,30 @@ class UserChangePasswordView(FormView):
                     email = self.request.POST.get('email')
                     user = User.objects.get(email=email)
                     new_password = self.request.POST.get('password')
-                    confirm_password = self.request.POST.get('confirm_password')
+                    confirm_password = self.request.POST\
+                        .get('confirm_password')
                     if new_password == confirm_password:
                         user.set_password(new_password)
                         user.save()
                         request.session['email'] = ''
-                        logger.info(f'{request.user.username} успешно изменил пароль')
-                        messages.success(self.request, _(f'Пароль пользователя {email} успешно изменен'))
+                        text_log = f'{request.user.username} ' \
+                                   f'успешно изменил пароль'
+                        logger.info(text_log)
+                        messages.success(self.request,
+                                         _(f'Пароль пользователя {email} '
+                                           f'успешно изменен'))
                     else:
-                        messages.error(self.request, _(f'Новый пароль пользователя {email} не подтвержден повторным вводом'))
+                        messages.error(self.request,
+                                       _(f'Новый пароль пользователя {email}'
+                                         f' не подтвержден повторным вводом'))
                 except User.DoesNotExist:
                     request.session['email'] = ''
-                    messages.error(self.request, 
-                                   _(f'Пользователь, имеющий e-mail {email} в системе не существует.'))
+                    messages.error(self.request,
+                                   _(f'Пользователь, имеющий e-mail {email} в '
+                                     f'системе не существует.'))
 
-        return super(UserChangePasswordView, self).dispatch(request, *args, **kwargs)
+        return super(UserChangePasswordView, self)\
+            .dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
 
@@ -65,7 +77,8 @@ class UserChangePasswordView(FormView):
             email = self.request.session['email']
         except KeyError:
             email = ''
-        context['change_password_form'] = UserChangePasswordForm(initial={'email': email})
+        context['change_password_form'] = UserChangePasswordForm(
+            initial={'email': email})
 
-       # Exit
+        # Exit
         return context
